67d4fc5685fed72aebceda544b1c4bd6d7f92f9c
Linkon <flinkon9@gmail.com>
2024-12-08 18:27:56 +0600

    vercel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -11,7 +12,7 @@ app.use(cors())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://Movie:rE2WdrNuKW5jrvFv@coffee.2vgrj.mongodb.net/?retryWrites=true&w=majority&appName=coffee";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@coffee.2vgrj.mongodb.net/?retryWrites=true&w=majority&appName=coffee`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
